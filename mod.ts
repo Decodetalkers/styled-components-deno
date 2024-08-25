abc4c3e4d49626410f3a79867d53cd1b3f04c8ec
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-08-25 21:53:26 +0859

    chore: bump version

--- mod.ts
+++ mod.ts
@@ -1,3 +1,20 @@
+/**
+ * @module
+ * This is a deno version styled-component, very simple version.
+ *
+ * @example
+ *
+ * ```typescript
+ * import styled from "@nobody/styled-component-deno";
+ *
+ * const Title = styled.div`
+ *    font-size: 4em;
+ *    text-align: center;
+ *    color: #BF4F74
+ *  `
+ * ```
+ */
+
 export * from "./styled.tsx";
 
 import { styled } from "./styled.tsx";
