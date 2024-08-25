ac674e0162377a4f7c4315e013c64e9b4462aa08
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-08-25 17:58:49 +0859

    chore: update

--- styled.tsx
+++ styled.tsx
@@ -1,3 +1,6 @@
+/** @jsxRuntime automatic */
+/** @jsxImportSource npm:preact */
+
 import {
   type ClassAttributes,
   h as createPreactElement,
@@ -5,6 +8,7 @@ import {
 } from "preact";
 
 import type React from "react";
+import { domElements, type SupportedHTMLElements } from "./domElements.ts";
 
 type Prop =
   & JSX.DOMAttributes<HTMLInputElement>
@@ -18,7 +22,6 @@ function createElement<T extends keyof JSX.IntrinsicElements>(
     props: JSX.IntrinsicElements[T],
   ) => {
     const { style, children, ...restProps } = props;
-    console.log(style);
 
     let set_style: typeof style = defaultStyle;
     if (style) {
@@ -32,144 +35,6 @@ function createElement<T extends keyof JSX.IntrinsicElements>(
   return Element;
 }
 
-const elements = [
-  "a",
-  "abbr",
-  "address",
-  "area",
-  "article",
-  "aside",
-  "audio",
-  "b",
-  "base",
-  "bdi",
-  "bdo",
-  "big",
-  "blockquote",
-  "body",
-  "br",
-  "button",
-  "canvas",
-  "caption",
-  "cite",
-  "code",
-  "col",
-  "colgroup",
-  "data",
-  "datalist",
-  "dd",
-  "del",
-  "details",
-  "dfn",
-  "dialog",
-  "div",
-  "dl",
-  "dt",
-  "em",
-  "embed",
-  "fieldset",
-  "figcaption",
-  "figure",
-  "footer",
-  "form",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "header",
-  "hgroup",
-  "hr",
-  "html",
-  "i",
-  "iframe",
-  "img",
-  "input",
-  "ins",
-  "kbd",
-  "keygen",
-  "label",
-  "legend",
-  "li",
-  "link",
-  "main",
-  "map",
-  "mark",
-  "menu",
-  "menuitem",
-  "meta",
-  "meter",
-  "nav",
-  "noscript",
-  "object",
-  "ol",
-  "optgroup",
-  "option",
-  "output",
-  "p",
-  "param",
-  "picture",
-  "pre",
-  "progress",
-  "q",
-  "rp",
-  "rt",
-  "ruby",
-  "s",
-  "samp",
-  "script",
-  "section",
-  "select",
-  "small",
-  "source",
-  "span",
-  "strong",
-  "style",
-  "sub",
-  "summary",
-  "sup",
-  "table",
-  "tbody",
-  "td",
-  "textarea",
-  "tfoot",
-  "th",
-  "thead",
-  "time",
-  "tr",
-  "track",
-  "u",
-  "ul",
-  "use",
-  "var",
-  "video",
-  "wbr", // SVG
-  "circle",
-  "clipPath",
-  "defs",
-  "ellipse",
-  "foreignObject",
-  "g",
-  "image",
-  "line",
-  "linearGradient",
-  "marker",
-  "mask",
-  "path",
-  "pattern",
-  "polygon",
-  "polyline",
-  "radialGradient",
-  "rect",
-  "stop",
-  "svg",
-  "text",
-  "tspan",
-] as const;
-
-export type SupportedHTMLElements = (typeof elements)[number];
-
 interface RenderFunc<T extends keyof JSX.IntrinsicElements> {
   (defaultStyle: TemplateStringsArray): React.Fc<JSX.IntrinsicElements[T]>;
 }
@@ -181,7 +46,6 @@ type Styled = {
 // deno-lint-ignore no-explicit-any
 const styledTmp: any = {};
 
-const domElements = new Set(elements);
 domElements.forEach((domElement) => {
   styledTmp[domElement] = (style: TemplateStringsArray) => {
     const style_css = style.join("");
